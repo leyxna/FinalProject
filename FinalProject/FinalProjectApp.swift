7023e742827d4f6f32d8f2bbd78c9104dcb057aa
Leyna <leynaphan16@gmail.com>
2023-07-14 09:37:19 -0500

    Initial Commit

---
+++ FinalProject/FinalProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinalProjectApp.swift
+//  FinalProject
+//
+//  Created by Leyna Phan on 7/14/23.
+//
+
+import SwiftUI
+
+@main
+struct FinalProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
